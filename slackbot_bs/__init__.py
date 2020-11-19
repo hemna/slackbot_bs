9e00c217c93a83f1612039ecfc3739b43fd781ec
Hemna <waboring@hemna.com>
2020-11-19 08:58:35 -0500

    added version_string

--- slackbot_bs/__init__.py
+++ slackbot_bs/__init__.py
@@ -1,5 +1,11 @@
 """Top-level package for SlackBot BS."""
+from pbr import version
+
+version_info = version.VersionInfo('openstack_exporter')
+version_string = version_info.version_string
 
 __author__ = """Walter A. Boring IV"""
 __email__ = 'waboring@hemna.com'
-__version__ = '0.1.0'
+__version__ = version_string
+
+
